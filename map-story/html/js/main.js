934e8870577698832d77168fa003b609300d4eb8
Chris Henrick <chrishenrick@gmail.com>
2014-02-12 12:19:00 -0500

    updated index & name spaced main.js

--- map-story/html/js/main.js
+++ map-story/html/js/main.js
@@ -1,141 +1,139 @@
-// Leaflet Stuff:
-// PCT center point is 40.3025, -121.234722
+myApp = {
 
-// store map config variables in an object
-var config = {
-  baselayer: new L.StamenTileLayer('terrain'),
-  initLatLng: new L.LatLng(40.3025, -121.2347),
-  initZoom: 5,
-  minZoom: 4,
-  maxZoom: 16,
-  zoomControl: true,
-  attributionControl: false
-}
+	map : '',
 
-// initialize map
-var map = L.map('map', {minZoom: config.minZoom, maxZoom: config.maxZoom, zoomControl: true, attributionControl: false})
+	renderMap : function(){
+		console.log('this: ', this);
+		var config = {
+				baselayer: new L.StamenTileLayer('terrain'),
+				initLatLng: new L.LatLng(40.3025, -121.2347),
+				initZoom: 5,
+				minZoom: 4,
+				maxZoom: 16,
+				zoomControl: false,
+				attributionControl: false
+			};
+		//init the map
+		this.map = L.map('map', config);
+		//disable drag and zoom handlers
+		this.map.dragging.disable();
+		this.map.touchZoom.disable();
+		this.map.doubleClickZoom.disable();
+		this.map.scrollWheelZoom.disable();
+		// disable tap handler, if present.
+		if (this.map.tap) this.map.tap.disable();
+		// add the Stamen terrain basemap layer
+		this.map.addLayer(config.baselayer);
+		// set init map center and zoom level
+		this.map.setView(config.initLatLng, config.initZoom);
+		// place attribution in bottom left so it's visible
+		new L.Control.Attribution({ position: 'bottomleft'}).addTo(this.map);
+	},
 
-// disable drag and zoom handlers
-// map.dragging.disable();
-// map.touchZoom.disable();
-// map.doubleClickZoom.disable();
-// map.scrollWheelZoom.disable();
-// disable tap handler, if present.
-// if (map.tap) map.tap.disable();
+	fetchData : function(){
+		var features,
+			len,
+			i,
+			lat,
+			lon,
+			poi,
+			markers;
 
-// add the Stamen base layer to the map 
-map.addLayer(config.baselayer);
+		// load poi.geojson data
+		$.getJSON('data/pct-poi-subset.geojson', function(data){
+			console.log('geojson data for POI\'s loaded: ', data);
 
-// set init map center and zoom
-map.setView(config.initLatLng, config.initZoom);
+			features = data.features,
+			len = features.length,
+			i = 0;
+			console.log('Fetch data says: features: ', features, ' len is: ', len);
 
-// place attribution in bottom left so it's visible
-// ** not working :( 
-new L.Control.Attribution({ position: 'bottomleft'}).addTo(map);
+			for (i; i<len; i++){
+				lat = features[i].geometry.coordinates[1];
+				lon = features[i].geometry.coordinates[0];
+				poi = new L.MakiMarkers.icon({
+					icon: "star",
+					color: "#b0b",
+					size: "s"
+				});
+				markers = new L.Marker([lat,lon],{
+					icon: poi
+				}).bindPopup("<h2>Point of Interest</h2>");
 
-// add PCT line feature from external geojson file
-$.getJSON("data/pct.geojson", function(data) {
-	
-	console.log("geojson for pct trail loaded");
-	
-	// styles to pass to 
-	var myStyle = {
-		"color": "#9400ff",
-		"weight": 4,
-		"opacity": 0.7,
-		"smoothFactor": 2
-	}
-	
-	// array to store lat lon values for animatedMarker
-	var temp = [];
+				// add the poi markers to the map
+				myApp.map.addLayer(markers);
+			}	
+		})
+		.done(function() {
+			console.log( "another success! getJSON for POI.geojson is done." );
+		})
+		.fail(function( jqxhr, textStatus, error ) {
+			var err = textStatus + ", " + error;
+			console.log( "Request Failed: " + err );
+		})    
+		.always(function() {
+			// run this stuff after, regardless of success or failure
+		}); // end $.getJSON(poi.geojson);
 
-	// loop through geojson data and push the lat lon values to line array
-	for (d in data) {
-		console.log(data);
-		var i = 0,
-			len = data.features.length;
-		for (i; i < len; i ++){
-			//console.log(data.features[i])
-			var j = 0,
-				coordinates = data.features[i].geometry.coordinates,
-				cLen = coordinates.length;
-			for (j; j<cLen; j++){
-				//console.log(coordinates[j]);
-				var reverseCoordinates = [];
+		// add PCT line feature from external geojson file
+		$.getJSON("data/pct.geojson", function(data) {
+			console.log("geojson for pct trail loaded: ", data);
 
-				//reverse the order of lat lon as leaflet reads lon then lat
-				reverseCoordinates.push(coordinates[j][1]);
-				reverseCoordinates.push(coordinates[j][0]);
-				temp.push(reverseCoordinates);
-			}
-		}
-	}
-	
-	// add pct lat lon points for the marker to animate
-	var line = L.polyline(temp);
-	animatedMarker = L.animatedMarker(line.getLatLngs(), {
-			autoStart: false,
-			distance: 2000,
-			interval: 1000
-		});
+			var myStyle = {
+				"color": "#9400ff",
+				"weight": 4,
+				"opacity": 0.7,
+				"smoothFactor": 2
+			},
+				temp = [];
+			// loop through geojson data and push the lat lon values to line array
 
-	map.addLayer(animatedMarker);
+			for (d in data) {
+				var i = 0,
+					l = data.features.length;
+				for (i; i < l; i ++){
+					var j = 0,
+						coordinates = data.features[i].geometry.coordinates,
+						cLen = coordinates.length;
+					for (j; j<cLen; j++){
+						var reverseCoordinates = [];
+							//reverse the order of lat lon as leaflet reads lon then lat
+						reverseCoordinates.push(coordinates[j][1]);
+						reverseCoordinates.push(coordinates[j][0]);
+						temp.push(reverseCoordinates);
+					}
+				}
+			} // end outer for loop
 
+			var line = L.polyline(temp);
+			animatedMarker = L.animatedMarker(line.getLatLngs(), {
+					autoStart: false,
+					distance: 2000,
+					interval: 1000
+				});
+			// add the animated marker
+			myApp.map.addLayer(animatedMarker);
+			// add pct line geojson layer
+			var pctLine = L.geoJson(data, {
+					style : myStyle
+				}).addTo(myApp.map); 
+			// add event listener for user scrolling
+			myApp.onScroll();
+			// add event listener for animatedMarker lat lon position
+			animatedMarker.on('move', myApp.onMove);
+		})
+		.done(function() {
+			console.log( "success! getJSON for pct.geojson is done." );
+		})
+		.fail(function( jqxhr, textStatus, error ) {
+			var err = textStatus + ", " + error;
+			console.log( "Request Failed: " + err );
+		})    
+		.always(function() {
+			// run this stuff after, regardless of success or failure
+		}); // end $.getJSON(pct.geojson);
 
-	//When GeoJSON is loaded
-	var geojsonLayer = L.geoJson(data, {
-			style: myStyle
-		}).addTo(map); //Add layer to map	
-
-	// grab the animated marker lat lon values (note: they are constantly changing)
-	console.log(animatedMarker['_latlng'].lat);
-	console.log(animatedMarker['_latlng'].lng);
-	console.log(animatedMarker);
-
-	// start browser panning interactivity:
-	markerCntrl.run();
-
-	// .on = action is turned on .off = action is turned off
-	animatedMarker.on('move', markerCntrl.onMove);
-
-});
-
-
-//Load the POI data
-$.getJSON('data/pct-poi-subset.geojson', function(data){
-	console.log('geojson data for POI\'s loaded');
-	console.log('POI data.features: ', data.features	);
-
-	var poiStyle = {
-			title: 'point of interest',
-		}
-	var features = data.features,
-		len = features.length,
-		i;
-	for (i=0; i<len; i++){
-		var lat = features[i].geometry.coordinates[1],
-			lon = features[i].geometry.coordinates[0];
-
-		var poi = new L.MakiMarkers.icon({
-			icon: "star",
-			color: "#b0b",
-			size: "s"
-		});
-
-		var markers = new L.Marker([lat,lon],{
-			icon: poi
-		}).bindPopup("<b>Point of Interest</b>");	
-
-		map.addLayer(markers);
-	}
-	
-
-})
-
-
-
-// namespace for marker control
-markerCntrl = {
+	},
 
 	coordinates : { //arbitrary right now for testing
 		start: [-116.46694979146261, 32.589707],
@@ -147,12 +145,12 @@ markerCntrl = {
 	markerRun: false,
 
 	start : function(){
-		if (this.markerRun === false){
+		if (!this.markerRun){
 			animatedMarker.start();
 		}
 	},
 	stop : function(){
-		if (this.markerRun === true){
+		if (this.markerRun){
 			animatedMarker.stop();
 		}		
 	},
@@ -160,20 +158,12 @@ markerCntrl = {
 	pan : function() {
 		var fps = 100;
 		setInterval(function(){
-			map.panTo({lon: animatedMarker['_latlng'].lng, lat: animatedMarker['_latlng'].lat})
-		},fps)
+			myApp.map.panTo({lon: animatedMarker['_latlng'].lng, lat: animatedMarker['_latlng'].lat})
+		},fps);
 	},
 
-	zoomIn : function(factor){
-		map.zoomIn(factor);
-	},
-
-	zoomOut : function(factor){
-		map.zoomOut(factor);
-	},
-
-	run : function() {
-		console.log('this.coordinates.one[0]: ' + markerCntrl.coordinates.one[0]);
+	onScroll : function() {
+		console.log('this.coordinates.one[0]: ' + myApp.coordinates.one[0]);
 	
 		$('#wp1').waypoint(function(direction) {
 
@@ -181,20 +171,15 @@ markerCntrl = {
 
 			switch(direction){
 				case 'down' :					
-					//animatedMarker.setLatLng({lat: markerCntrl.coordinates.start[1], lon: markerCntrl.coordinates.start[0]})				
-					map.zoomIn(6);
-					markerCntrl.start();
-			  		var fps = 100,
-						panTo = setInterval(function(){
-							map.panTo({lon: animatedMarker['_latlng'].lng, lat: animatedMarker['_latlng'].lat})
-						},fps);
-					markerCntrl.markerRun = true;
+					myApp.map.zoomIn(6);
+					myApp.start();
+					myApp.pan();
+					myApp.markerRun = true;
 					break;
 				case 'up' :
-					map.zoomOut(6)
-					markerCntrl.stop();
-					markerCntrl.markerRun = false;
-					//animatedMarker.setLatLng({lat: markerCntrl.coordinates.start[1], lon: markerCntrl.coordinates.start[0]})
+					myApp.map.zoomOut(6)
+					myApp.stop();
+					myApp.markerRun = false;					
 					break;
 			}
 
@@ -203,34 +188,31 @@ markerCntrl = {
 	},
 
 	onMove : function(e){
-		var lat1 = markerCntrl.coordinates.one[1],
-			lng1 = markerCntrl.coordinates.one[0];
+		var lat1 = myApp.coordinates.one[1],
+			lng1 = myApp.coordinates.one[0];
+		myApp.checkMapBounds(e, lat1, lng1);
+	},
 
-		// log e / this properties to debug
-		// console.log('e.latlng.lat: ' + e.latlng.lat + ' e.latlng.lng: ' + e.latlng.lng);
-		// console.log('e is: ' + e);
-		// console.log('lat1: ' + lat1);
-		// console.log('lng1: ' + lng1);
-		// console.log('this latlon.lat: ' + this['_latlng'].lat);
-		// console.log('this latlon.lng: ' + this['_latlng'].lng);
+	checkMapBounds : function(e, lat, lon) {
+		if (e.latlng.lng === lon && e.latlng.lat === lat){
+			alert("whoa!"); //works
+			console.log("lat lon check worked!");
+			myApp.stop();
+			myApp.markerRun = false;
+		}
+	},
 
-		if (e.latlng.lng === lng1 && e.latlng.lat === lat1){
-				alert("whoa!"); //works
-				console.log("lat lon check worked!");
-				//animatedMarker.setLatLng({lat: lat1, lon: lng1}); // works but then animateMarker.start() won't work
-				animatedMarker.stop(); // doesn't work, not sure why. Possibly due to a conflict with JQuery Waypoints?
-				
-				// thought this was working but apparently not
-				e.latlng.lat = lat1;
-				e.latlng.lng = lng1;
-
-				markerCntrl.markerRun = false;
-			}
+	init : function() {
+		console.log('here we go');
+		//draw map
+		myApp.renderMap();
+		//async load geo data
+		myApp.fetchData();
+		//attach scroll events
+		//start tracking the marker for lat/long pos
 	}
-}
+
+} //end myApp
 
 
-
-
-
-
+window.onload = myApp.init;
